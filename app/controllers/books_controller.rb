335b4785e9103b268a9c2fc30c903537bc3884f3
rachel_giacobozzi <rgiacobozzi@gmail.com>
2014-04-08 20:02:02 -0400

    ActiveRecord queries

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,16 +16,22 @@ end
 
 def create
   @book = Book.new(book_params)
-  @book.save
-  redirect_to @book
+      if @book.save
+        redirect_to @book, notice: "#{@book.title} was created!"
+      else
+        render :new
+      end
 end
 
 def edit
  end
 
 def update
-  @book.update(book_params)
-  redirect_to @book
+  if @book.update(book_params)
+        redirect_to @book, notice: "#{@book.title} was created!"
+      else
+        render :new
+      end
 end
 
 def destroy
